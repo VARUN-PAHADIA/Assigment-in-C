b3f0907cbd85763b18246096ad965b4746ba889a
varun pahadia <pahadiavarun02@gmail.com>
2026-09-04 23:36:52 +0530

    Q18 complete

---
+++ Assigment__1/Q18_earthTime.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int n =31558150, days , hours , minutes ;
+    days = n/(24*60*60);
+    n=n%(365*24*60*60);
+    hours = n/(60*60);
+    n=n%(60*60);
+    minutes= n/(60);
+    printf("days=%d : hours=%d : minutes=%d", days , hours, minutes);
+    
+
+}
